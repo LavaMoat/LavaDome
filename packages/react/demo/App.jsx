5b2588cc9172f9a4c52f54fc124a44fec4642333
Jongsun Suh <jongsun.suh@icloud.com>
2023-12-21 22:39:08 -0800

    fix(demo): set up demo react app
    - attach react app to root
    - move `index.html` content into `App`

--- packages/react/demo/App.jsx
+++ packages/react/demo/App.jsx
@@ -1,6 +1,20 @@
 import React from 'react'
-import {LavaDome as LavaDomeReact} from "../src/index"
 
-export default function App({secret}) {
-    return <LavaDomeReact text={secret} />
+export default function App() {
+    return (
+        <div style={{ borderStyle: 'solid', margin: '10px', padding: '10px' }} >
+            <div>
+                This is not a secret:
+                <p id="PUBLIC">
+                    <span> PUBLIC_CONTENT_NOT_ONLY_ACCESSIBLE_TO_LAVADOME </span>
+                </p>
+            </div>
+            <div>
+                This is a secret:
+                <p id="PRIVATE">
+                    <span> TO_BE_REPLACED </span>
+                </p>
+            </div>
+        </div>
+    )
 }
