a3e7355dbb9952ee44cc703906cc3aa1204da02c
naugtur <naugtur@gmail.com>
2024-01-22 11:10:27 +0100

    fix: move envelope from react to core for defensive coding

--- packages/core/src/lavadome.mjs
+++ packages/core/src/lavadome.mjs
@@ -8,20 +8,48 @@ import {
     createElement,
     appendChild,
     textContentSet,
+    WeakMap, create, set, get
 } from './native.mjs';
 import {distraction, unselectable} from './element.mjs';
 import {getShadow} from './shadow.mjs';
+
+
+// there's no code running earlier 
+const STORE = new WeakMap();
+
+export const envelope = (secret) => {
+    const en = create(null);
+    set(STORE, en, secret);
+    return en;
+};
+
 
 export function LavaDome(host, opts) {
     opts = options(opts);
     
     // make exported API tamper-proof
-    defineProperties(this, {text: {value: text}});
+    defineProperties(this, {
+        text: {value: text, writable: false, configurable: false},
+        envelope: {value: envelope, writable: false, configurable: false},
+    });
 
     // child of the shadow, where the secret is set, must be unselectable
     const child = unselectable();
     const shadow = getShadow(host, opts);
     appendChild(shadow, child);
+
+    function envelope(anEnvelope) {
+        if (typeof anEnvelope !== 'object') {
+            throw new Error(
+                `LavaDome: first argument must be an object, instead got ${stringify(anEnvelope)}`);
+        }
+        const secret = get(STORE, anEnvelope);
+        if (secret === undefined) {
+            throw new Error(
+                `LavaDome: first argument must be an envelope`);
+        }
+        text(secret);
+    }
 
     function text(text) {
         if (typeof text !== 'string') {
--- packages/react/src/index.jsx
+++ packages/react/src/index.jsx
@@ -1,12 +1,6 @@
 import React, { useEffect, useRef } from 'react'
 import { LavaDome as LavaDomeCore } from "@lavamoat/lavadome-core"
-
-const STORE = new WeakMap();
-export const envelope = (secret) => {
-    const en = Object.create(null);
-    STORE.set(en, secret);
-    return en;
-};
+export { envelope } from "@lavamoat/lavadome-core"
 
 export const LavaDome = ({ envelope, unsafeOpenModeShadow }) => {
     const hostRef = useRef(null);
@@ -32,7 +26,7 @@ function LavaDomeShadow({ hostRef, envelope, unsafeOpenModeShadow }) {
 
     useEffect(() => {
         const payload = STORE.get(envelope);
-        lavadome.current.text(payload);
+        lavadome.current.envelope(payload);
     }, [envelope]);
 
     return <></>
